81442819904ab1ce9aae031ee91ff06ed85b0a50
nicoledodge <nicoledodge5@gmail.com>
2021-11-10 16:07:20 -0600

    pushing updated delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,10 +18,34 @@ app.get("/notes", (req, res) => {
 app.post("/notes", (req, res) => {
     req.body.id = uniqid()
     dbjson.push(req.body)
-    fs.writeFile(__dirname, "/../db/db.json", JSON.stringify(dbjson), err => {
+    fs.writeFile(__dirname, '/../db/db.json', JSON.stringify(dbjson), err => {
         if(err) throw (err)
     })
     res.end()
-})
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+
+        let filtered;
+        if (err) {
+            console.error(err);
+        } else {
+
+            const currentNoteID = req.params.id;
+
+            const notes = JSON.parse(data);
+
+            filtered = notes.filter(note => note.id !== currentNoteID);
+
+            fs.writeFile('../db/db.json', JSON.stringify(filtered, null, 4), (writeErr) =>
+                writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+            );
+
+        }
+    });
+});
 
 module.exports = app;
